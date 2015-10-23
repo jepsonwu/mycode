50705978830543c214760dda98be1a9292eacf59
jepson <jepson@abc360.com>
2015-10-23 11:44:44 +0800

    hah

--- index.php
+++ index.php
@@ -1,101 +1,89 @@
 <?php
-$key="8a476f61b64f34ec0b4f7f4c5aef0d1b";
-$private_key_path="self_private_key.pem";
-
-$url="http://dev.hapigou.com/V2/users/auth_demo";
-//todo url_encode
-//测试demo
-$param=array(
-	"timestamp"=>time(),
-	"user_id"=>59
+$url = "http://hpg.com/v2/upload/pic";
+$file_name = "wap.zip";
+$data = array(
+	"user_id"=>1,
+	"feedback_id"=>2,
+	"data"=>base64_encode(file_get_contents($file_name))
 );
 
-//$sign=rsaSign(createParam(sortParam(filterParam($param))),$private_key_path);
-$sign=md5(createParam(sortParam(filterParam($param))).$key);
-$param['sign']=$sign;
-$param['sign_type']="MD5";
+$result = curl($url, "POST", $data);
+var_dump($result);
 
-echo $url."?".createParamUrlencode($param);
-$result=curl($url."?".createParamUrlencode($param));
-//$result=curl("
-http://dev.hapigou.com/v2/users/auth_demo?timestamp=1441003656&user_id=59&sign_type=MD5&sign=3023fbb9f212cbe0071db3216e55aa60");
+//$fp=fopen('data://text/plain;base64,','r');
+//$meta=stream_get_meta_data($fp);
+//print_r($meta);
+//echo  file_get_contents ( 'data://text/plain;base64,SSBsb3ZlIFBIUAo=' );
+//php 重定向标准输入输出
+//while (true) {
+//	$line = fgets(STDIN);//	if ($line == "exit".PHP_EOL)
+//		break;
+//	echo $line;
+//}
 
-var_dump($result);exit;
-
-function createParamUrlencode($data) {
-		$param  = "";
-		while (list ($key, $val) = each ($data)) {
-			$param.=$key."=".urlencode($val)."&";
-		}
-		//去掉最后一个&字符
-		$param = substr($param,0,count($param)-2);
-		
-		//如果存在转义字符，那么去掉转义
-		if(get_magic_quotes_gpc()){$param = stripslashes($param);}
-		
-		return $param;
-	}
-
-function rsaSign($data, $private_key_path) {
-		$private_key=file_get_contents($private_key_path);
-
-	    $res = openssl_pkey_get_private($private_key);
-	    openssl_sign($data, $sign, $res);
-	    openssl_free_key($res);
-	    
-		//base64编码
-	    $sign = base64_encode($sign);
-	    return $sign;
-	}
-
-function createParam($data){
-	$param="";
-
-	foreach ($data as $key => $value) {
-		$param.=$key."=".$value."&";
-	}
-	$param=trim($param,"&");
-
-	//转义字符
-
-	return $param;
-}
-
-function filterParam($data){
-	$return=array();
-
-	foreach ($data as $key => $value) {
-		if($key=="sign"||$key=="sign_type"||$key=="") 
-			continue;
-		else
-			$return[$key]=trim($value);
-	}
-
-	return $return;
-}
-
-function sortParam($data){
-	ksort($data);
-	reset($data);
-
-	return $data;
-}
+//fclose(STDOUT);
+//$STDOUT=fopen("text.log","a");
+//echo "a";
 
 
-function curl($url,$type="GET",$data=null,$header=null,$option=null){
-	$ch=curl_init();
+// $key="8a476f61b64f34ec0b4f7f4c5aef0d1b";
+// $private_key_path="self_private_key.pem";
+// $service_public_key_path="service_public_key.pem";
 
-	$options=array(
-		CURLOPT_URL=>$url,
-		CURLOPT_TIMEOUT=>10,
-		CURLOPT_RETURNTRANSFER=>true,
+// $url="http://hpg.com/V2/users/auth_demo";
+// //todo url_encode
+// //测试demo
+// $param=array(
+// 	"timestamp"=>time(),
+// 	"app_id"=>"20150001",
+// 	"encrypt_type"=>"RSA",
+// );
+
+// $data=array(
+// 	"user_id"=>1,
+// 	"name"=>"wujp"
+// );
+// $param['encrypt_data']=rsaEncrypt(createParam(sortParam($data)),$service_public_key_path);
+
+// //$sign=rsaSign(createParam(sortParam(filterParam($param))),$private_key_path);
+// $sign=md5(createParam(sortParam(filterParam($param))).$key);
+// // echo $sign;exit;
+// $param['sign']=$sign;
+// $param['sign_type']="MD5";
+
+// //echo $url."?".createParamUrlencode($param)."\n";
+
+// $result=curl($url."?".createParamUrlencode($param));
+// var_dump($result);exit;
+//$filename="abc360_access.log";
+//$file=file_get_contents($filename);
+//
+//$data=array(
+//	"user_id"=>59,
+//	"feedback_id"=>1,
+//	"start_time"=>"1442115258",
+//	"end_time"=>"1442201613",
+//	"file"=>urlencode($file),
+//);
+//$result=curl("http://dev.hapigou.com/V2/upload/wap_logs","POST",$data);
+//var_dump($result);
+
+
+function curl($url, $type = "GET", $data = null, $header = null, $option = null)
+{
+	$ch = curl_init();
+
+	$options = array(
+		CURLOPT_URL => $url,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_RETURNTRANSFER => true,
 	);
 
-	!is_null($option)&&is_array($option)&&$options=array_merge($options,$option);
+	!is_null($option) && is_array($option) && $options = array_merge($options, $option);
 
-	curl_setopt_array($ch,$options);
+	curl_setopt_array($ch, $options);
 
-	switch (strtoupper($type)){
+	switch (strtoupper($type)) {
 		case 'GET':
 			curl_setopt($ch, CURLOPT_HTTPGET, true);
 			break;
@@ -104,7 +92,7 @@ function curl($url,$type="GET",$data=null,$header=null,$option=null){
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 			break;
 		case 'HEAD':
-			
+
 			break;
 		case 'PUT':
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
@@ -121,11 +109,11 @@ function curl($url,$type="GET",$data=null,$header=null,$option=null){
 	// else
 	// 	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 
-	$return=curl_exec($ch);
-	if($return!==false){
+	$return = curl_exec($ch);
+	if ($return !== false) {
 		echo curl_getinfo($ch)['http_code'];
-	}else{
-		$return="ERR:(".curl_errno($ch).")".curl_error($ch);
+	} else {
+		$return = "ERR:(" . curl_errno($ch) . ")" . curl_error($ch);
 	}
 
 	curl_close($ch);
@@ -133,25 +121,107 @@ function curl($url,$type="GET",$data=null,$header=null,$option=null){
 	return $return;
 }
 
-function curl_multi(){
+function curl_multi()
+{
 
 }
 
-function curl_ftp(){
+function curl_ftp()
+{
 
 }
 
-function curl_ssl(){
+function curl_ssl()
+{
 
 }
 
-function curl_file(){
+function curl_file()
+{
 
 }
 
-function curl_proxy(){
+function curl_proxy()
+{
 
 }
 
+//pkcs1
+function rsaEncrypt($content, $public_key_path)
+{
+	$public_key = file_get_contents($public_key_path);
+	$public_key = openssl_pkey_get_public($public_key);
+
+	$result = "";
+	openssl_public_encrypt($content, $result, $public_key);
+
+	return base64_encode($result);
+}
+
+function createParamUrlencode($data)
+{
+	$param = "";
+	while (list ($key, $val) = each($data)) {
+		$param .= $key . "=" . urlencode($val) . "&";
+	}
+	//去掉最后一个&字符
+	$param = substr($param, 0, count($param) - 2);
+
+	//如果存在转义字符，那么去掉转义
+	if (get_magic_quotes_gpc()) {
+		$param = stripslashes($param);
+	}
+
+	return $param;
+}
+
+function rsaSign($data, $private_key_path)
+{
+	$private_key = file_get_contents($private_key_path);
+
+	$res = openssl_pkey_get_private($private_key);
+	openssl_sign($data, $sign, $res);
+	openssl_free_key($res);
+
+	//base64编码
+	$sign = base64_encode($sign);
+	return $sign;
+}
+
+function createParam($data)
+{
+	$param = "";
+
+	foreach ($data as $key => $value) {
+		$param .= $key . "=" . $value . "&";
+	}
+	$param = trim($param, "&");
+
+	//转义字符
+
+	return $param;
+}
+
+function filterParam($data)
+{
+	$return = array();
+
+	foreach ($data as $key => $value) {
+		if ($key == "sign" || $key == "sign_type" || $key == "")
+			continue;
+		else
+			$return[$key] = trim($value);
+	}
+
+	return $return;
+}
+
+function sortParam($data)
+{
+	ksort($data);
+	reset($data);
+
+	return $data;
+}
 
 
--- python/index.py
+++ python/index.py
@@ -1,94 +1,23 @@
-#!/usr/bin/env python3
-#-*- coding:utf-8 -*-
+#!/usr/bin/env python2
+#coding:utf-8
+import urllib
+import re
+def getHtml(url):
+	page=urllib.urlopen(url)
+	html=page.read()
+	return html
 
+def getImg(html):
+	reg=r'src="(.+?\.jpg)" pic_ext'
+	img_reg=re.compile(reg)
+	img_list=img_reg.findall(img_reg,html)
+	print img_list
+	return None
 
-#input print
+	x=0
+	for img_url in img_list:
+		urllib.urlretrieve(img_url,'%s.jpg' % x)
+		x+=1
 
-# name=input("please input your name:")
-
-# print("hello ",name)
-
-
-#python 大小写敏感
-
-a=10
-
-if a>1:
-	print(a)
-else:
-	print(-a)
-
-#中文测试
-print("中文测试")
-
-#数据类型
-print(1.23e9)
-
-#''' ''' 特殊换行  \n也行
-print('''this one line\nthis two line
-this three line''')
-
-#True False  注意大小写
-print(True)
-
-#特殊字符  None  null
-print(None)
-
-#常量  常量的值还是可以改变的
-TP='aaa'
-print(TP)
-
-TP='bbb'
-print(TP)
-
-#四则运算
-
-#除法 结果为浮点数
-print(10/3)
-print(10/2)
-
-#取整除
-print(10//3)
-
-#取余
-print(10%3)
-
-#格式化字符串
-str_var="test"
-print("this is %s %%" % str_var)
-
-#字符编码转换
-
-#有序集合
-
-#list 集合  可变
-class_list=["a","b","c"]
-print(class_list)
-print(class_list[0],class_list[-2])
-
-print(len(class_list))
-
-class_list.append("f")
-print(class_list)
-
-class_list.insert(1,"rr")
-print(class_list)
-
-class_list.pop()
-print(class_list)
-
-class_list.pop(3)
-print(class_list)
-
-#tuple集合  不可变
-class_list=("dd","dd","eee")
-print(class_list)
-
-# class_list.append("rrr")
-# print(class_list)
-
-#一个集合 加上， 区分
-class_list=("ee",)
-print(class_list)
-
-
+html=getHtml("http://tieba.baidu.com/p/2460150866")
+#print html
